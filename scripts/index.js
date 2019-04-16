559d05ab572d78b7417539dc36d927c4d2e3e743
Aiden Storey <aiden.derooy@gmail.com>
2019-04-15 23:11:02 -0400

    Refactor index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
+// ----------
+// Constants
+// ----------
+
+const DIRECTIONAL_OFFSET = {
+    'Up': { x: 0, y: -1 },
+    'Down': { x: 0, y: 1 },
+    'Left': { x: -1, y: 0 },
+    'Right': { x: 1, y: 0 },
+};
+
+const INVERSE_DIRECTION = {
+    'Up': 'Down',
+    'Down': 'Up',
+    'Left': 'Right',
+    'Right': 'Left',
+};
+
+const MOVEMENT_DISTANCE = 1.5;
+
+
 // ---------
 // Elements
 // ---------
@@ -7,24 +28,80 @@ const linkedin = document.getElementById('l');
 const snake = document.getElementById('s');
 
 
-// ------------------
-// Handle User Input
-// ------------------
+// ----------
+// Variables
+// ----------
 
-const INVERSE_DIRECTION = {
-    'Up': 'Down',
-    'Down': 'Up',
-    'Left': 'Right',
-    'Right': 'Left',
-};
+let resizeTaskId, updateTaskId;
 
-let currentDirection = 'Right';
+let elementOriginX, elementOriginY;
+
+let playingOriginX, playingOriginY;
+let playingHeight, playingWidth;
+
+let screenHeight, screenWidth;
+let screenEM;
+
+let snakeCollectables = [ github, linkedin ];
+let snakeParts = [];
+let snakeStart, snakeLast;
+
+let snakeCurrentDirection, snakeNextDirection;
+
+
+// --------
+// Helpers
+// --------
 
 const changeDirection = (direction) => {
-    if (currentDirection !== direction && currentDirection !== INVERSE_DIRECTION[direction]) {
-        currentDirection = direction;
+    if (snakeCurrentDirection !== direction && snakeCurrentDirection !== INVERSE_DIRECTION[direction]) {
+        snakeNextDirection = direction;
+    }
+}
+
+const elementMove = (element, x, y) => {
+    element.style.position = 'fixed';
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+};
+
+const elementReset = (element) => {
+    element.style.position = 'static';
+}
+
+const snakeAdd = (count = 1) => {
+    for (let c = 0; c < count; ++c) {
+        let element
+        if (snakeParts.length === 0) {
+            element = snake.cloneNode();
+            snakeParts.push(element);
+        } else {
+            element = snakeParts[snakeLast].cloneNode();
+            snakeParts.splice(snakeLast + 1, 0, element);
+            snakeLast += 1;
+        }
+        document.body.appendChild(element);
+        element.style.background = '#333';
     }
 };
+
+const snakeCheckCollision = () => {
+    const head = snakeParts[snakeStart];
+
+    if (head.offsetLeft < playingOriginX || head.offsetLeft > playingOriginX + playingWidth ||
+        head.offsetTop < playingOriginY || head.offsetTop > playingOriginY + playingHeight) {
+            return true;
+        }
+
+    // TODO: Self collisions
+
+    return false;
+}
+
+
+// ----------
+// Listeners
+// ----------
 
 addEventListener('keydown', (event) => {
     if (event.key.startsWith('Arrow')) {
@@ -33,240 +110,99 @@ addEventListener('keydown', (event) => {
     }
 });
 
-// ---------------
-// Handle element
-// ---------------
+// TODO: swipe listener
 
-const elementReset = (element) => {
-    element.style.position = 'static';
-}
+addEventListener('resize', () => {
+    if (resizeTaskId !== null) {
+        clearTimeout(resizeTaskId);
+    } else {
+        destroy();
+    }
 
-const elementSetPosition = (element, x, y) => {
-    element.style.position = 'fixed';
-    element.style.left = `${x}px`;
-    element.style.top = `${y}px`;
+    resizeTaskId = setTimeout(() => {
+        resizeTaskId = null;
+        initialize();
+    }, 500);
+});
 
-    element.data = {
-        x: (x - screenOriginX) / screenEM,
-        y: (y - screenOriginY) / screenEM,
-    };
+
+// -----
+// Game
+// -----
+
+const initialize = () => {
+    resizeTaskId = updateTaskId = null;
+
+    elementOriginX = github.offsetLeft;
+    elementOriginY = github.offsetTop;
+
+    snake.style.position = 'fixed';
+    snake.style.left = '1rem';
+    screenEM = snake.offsetLeft * MOVEMENT_DISTANCE;
+
+    screenHeight = document.documentElement.clientHeight;
+    screenWidth = document.documentElement.clientWidth;
+
+    playingOriginX = elementOriginX - Math.floor(elementOriginX / screenEM) * screenEM;
+    playingOriginY = elementOriginY - Math.floor(elementOriginY / screenEM) * screenEM;
+    playingHeight = Math.floor((screenHeight - playingOriginY) / screenEM) * screenEM - screenEM;
+    playingWidth = Math.floor((screenWidth - playingOriginX) / screenEM) * screenEM - screenEM;
+
+    elementMove(github, elementOriginX, elementOriginY);
+    elementMove(linkedin, elementOriginX + screenEM, elementOriginY);
+    elementMove(snake, elementOriginX + screenEM * 2, elementOriginY);
+
+    snakeCurrentDirection = snakeNextDirection = 'Right';
+    snakeStart = snakeLast = 0;
+
+    snakeAdd(3);
+
+    requestAnimationFrame(updateLoop);
 };
 
-
-// -----------------------
-// Handle screen resizing
-// -----------------------
-
-const MOVEMENT_DISTANCE = 1.5;
-
-let elementOriginX, elementOriginY;
-let screenHeight, screenWidth;
-
-let screenEM;
-
-let screenOriginX, screenOriginY;
-let screenHeightEM, screenWidthEM;
-
-let snakeOriginX, snakeOriginY;
-
-const adjustSize = () => {
-    if (document.documentElement.clientHeight !== screenHeight ||
-        document.documentElement.clientWidth !== screenWidth) {
-            screenHeight = document.documentElement.clientHeight;
-            screenWidth = document.documentElement.clientWidth;
-
-            elementReset(github);
-            elementOriginX = github.offsetLeft;
-            elementOriginY = github.offsetTop;
-
-            snake.style.position = 'fixed';
-            snake.style.left = '1rem';
-
-            screenEM = snake.offsetLeft * MOVEMENT_DISTANCE;
-
-            snake.style.position = null;
-            snake.style.left = null;
-
-            screenOriginX = elementOriginX - Math.floor(elementOriginX / screenEM) * screenEM;
-            screenOriginY = elementOriginY - Math.floor(elementOriginY / screenEM) * screenEM;
-            screenHeightEM = Math.floor((screenHeight - screenOriginY) / screenEM) * screenEM - screenEM;
-            screenWidthEM = Math.floor((screenWidth - screenOriginX) / screenEM) * screenEM - screenEM;
-
-            elementSetPosition(github, elementOriginX, elementOriginY);
-            elementSetPosition(linkedin, elementOriginX + screenEM, elementOriginY);
-            elementSetPosition(snake, elementOriginX + screenEM * 2, elementOriginY);
-
-            snakeStartX = snake.offsetLeft;
-            snakeStartY = snake.offsetTop;
-        return true;
-    }
-    return false;
-};
-
-
-// ----------------
-// Utility helpers
-// ----------------
-
-const FRAME_WAIT = 0.2;
-
-let deltaTime = 0.0;
-let previousTime = Date.now();
-let currentFrameTime = 0.0;
-
-const isPosition = (ax, ay, bx, by) => {
-    return ax === bx && ay === by;
-}
-
-const tick = () => {
-    const currentTime = Date.now();
-    deltaTime = (currentTime - previousTime) / 1000.0;
-    previousTime = currentTime;
-};
-
-
-// ------
-// Snake
-// ------
-
-const DIRECTIONAL_OFFSET = {
-    'Up': { x: 0, y: -1 },
-    'Down': { x: 0, y: 1 },
-    'Left': { x: -1, y: 0 },
-    'Right': { x: 1, y: 0 },
-}
-
-const snakeCollidables = [github, linkedin];
-const snakeParts = [];
-let snakeStart = 0;
-let snakeLast = 0;
-
-const snakeAddPart = (...elements) => {
-    for (const element of elements) {
-        if (snakeParts.length === 0) {
-            element.data = Object.assign({}, snake.data);
-            snakeParts.push(element);
-        } else {
-            element.data = Object.assign({}, snakeParts[snakeLast].data);
-            snakeParts.splice(snakeLast + 1, 0, element);
-
-            snakeRenderPart(element);
-            snakeLast +=1;
-        }
-    }
-}
-
-const snakeCloneElement = (ready = true) => {
-    const element = snake.cloneNode();
-    element.data = Object.assign({}, snake.data);
-
-    element.id = 'part-copy';
-
-    element.style.background = '#333';
-    element.style.position = 'fixed';
-    document.body.appendChild(element);
-
-    return element
-}
-
-const snakeCheckCollect = () => {
-    const head = snakeParts[snakeStart];
-
-    for (const collidable of snakeCollidables) {
-        if (head.data.x === collidable.data.x && head.data.y === collidable.data.y) {
-            snakeAddPart(snakeCloneElement());
-            elementSetPosition(collidable,
-                screenOriginX + (Math.floor(Math.random() * screenWidthEM / screenEM)) * screenEM,
-                screenOriginY + (Math.floor(Math.random() * screenHeightEM / screenEM)) * screenEM,
-            );
-        }
-    }
-}
-
-const snakeCheckCollision = () => {
-    const head = snakeParts[snakeStart];
-
-    if (head.offsetLeft < screenOriginX || head.offsetLeft > screenOriginX + screenWidthEM ||
-        head.offsetTop < screenOriginX || head.offsetTop > screenOriginY + screenHeightEM) {
-        return true;
-    }
-
-    // for (let partIndex = 0; partIndex < snakeParts.length; ++partIndex) {
-    //     if (partIndex !== snakeStart) {
-    //         const part = snakeParts[partIndex];
-    //         if (head.data.x === part.data.x && head.data.y === part.data.y) {
-    //             console.log("now");
-    //             return true;
-    //         }
-    //     }
-    // }
-
-    return false;
-}
-
-const snakeLastIndex = () => {
-    return (snakeStart + snakeParts.length - 1) % snakeParts.length;
-}
-
-const snakeMove = () => {
-    snakeLast = snakeLastIndex();
-
-    const part = snakeParts[snakeLast];
-
-    part.data.x = snakeParts[snakeStart].data.x + DIRECTIONAL_OFFSET[currentDirection].x;
-    part.data.y = snakeParts[snakeStart].data.y + DIRECTIONAL_OFFSET[currentDirection].y;
-    part.ready = true;
-
-    snakeRenderPart(part);
-
-    snakeStart = snakeLast;
-}
-
-const snakeRenderPart = (part) => {
-    part.style.left = `${screenOriginX + part.data.x * screenEM}px`;
-    part.style.top = `${screenOriginY + part.data.y * screenEM}px`;
-}
-
-const snakeReset = () => {
+const destroy = () => {
+    console.log(snakeParts);
     for (const part of snakeParts) {
         document.body.removeChild(part);
     }
-
-    currentDirection = 'Right';
-    snakeLast = 0;
-    snakeStart = 0;
     snakeParts.length = 0;
 
-    snakeAddPart(snakeCloneElement(), snakeCloneElement(false), snakeCloneElement(false));
+    elementReset(github);
+    elementReset(linkedin);
+    elementReset(snake);
 
-    for (let collidableIndex = 0; collidableIndex < snakeCollidables.length; ++collidableIndex) {
-        elementSetPosition(
-            snakeCollidables[collidableIndex],
-            elementOriginX + screenEM * collidableIndex,
-            elementOriginY
-        );
-    }
-}
+    clearTimeout(updateTaskId);
+    updateTaskId = null;
+};
 
-const update = () => {
-    const screenAdjusted = adjustSize();
-
-    tick();
-    currentFrameTime = Math.max(0.0, currentFrameTime - deltaTime);
-    if (currentFrameTime) {
-        return requestAnimationFrame(update);
-    }
-
-    if (!screenAdjusted && !snakeCheckCollision()) {
-        snakeCheckCollect();
-        snakeMove();
+const updateLoop = () => {
+    if (snakeCheckCollision()) {
+        destroy();
+        initialize();
     } else {
-        snakeReset();
+        snakeCurrentDirection = snakeNextDirection;
+        snakeLast = (snakeStart + snakeParts.length - 1) % snakeParts.length;
+        elementMove(
+            snakeParts[snakeLast],
+            snakeParts[snakeStart].offsetLeft + DIRECTIONAL_OFFSET[snakeCurrentDirection].x * screenEM,
+            snakeParts[snakeStart].offsetTop + DIRECTIONAL_OFFSET[snakeCurrentDirection].y * screenEM,
+        );
+        snakeStart = snakeLast;
+
+        for (const collectable of snakeCollectables) {
+            if (snakeParts[snakeStart].offsetLeft === collectable.offsetLeft &&
+                snakeParts[snakeStart].offsetTop === collectable.offsetTop) {
+                    snakeAdd();
+                    elementMove(
+                        collectable,
+                        playingOriginX + (Math.floor(Math.random() * playingWidth / screenEM)) * screenEM,
+                        playingOriginY + (Math.floor(Math.random() * playingHeight / screenEM)) * screenEM,
+                    );
+                }
+        }
+
+        updateTaskId = setTimeout(() => { requestAnimationFrame(updateLoop); }, 200);
     }
-
-    currentFrameTime = FRAME_WAIT;
-
-    requestAnimationFrame(update);
 };
 
 
@@ -274,6 +210,4 @@ const update = () => {
 // Start
 // ------
 
-adjustSize();
-snakeAddPart(snakeCloneElement(), snakeCloneElement(false), snakeCloneElement(false));
-requestAnimationFrame(update);
+initialize();
